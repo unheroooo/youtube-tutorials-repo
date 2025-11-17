0491099f7f63c3aa583ab01506e2d4f06fcf4f1d
unheroooo <jasonli.ty@gmail.com>
2025-11-17 17:41:17 +0800

    Fix node-sass compatibility: Replace deprecated node-sass with modern sass dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,14 +8,5 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-less`,
-      options: {
-        javascriptEnabled: true,
-        modifyVars: {
-          "primary-color": "#00BFA6",
-        },
-      },
-    },
   ],
 }
